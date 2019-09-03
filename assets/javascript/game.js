5d7b2beaddcac556f0a8301b5f417fdfd28ddb36
Ron Melendres <Ron.melendres25@gmail.com>
2019-09-03 13:37:49 -0700

    trying to fix click/scoring tie

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,115 +1,206 @@
-// 1: When you initialize game:
-    // a: wins and losses are set to 0.
-    // b: score total is set to 0.
-    // c: target number is randomly set.
-    // d: number vaule for each gem is set, however the vaules are hidden from the player.
-
 var wins = 0;
 var losses = 0;
 var scoreTotal = 0;
-var gemValue = [];
-var randomTargetNumber =[Math.floor(Math.random())];
+var randomTargetNumber = [Math.floor(Math.random())];
 // [Math.floor(Math.random() * 120 - 19) + 19]
 
+// var randomGemValue = [Math.floor(Math.random())];
 
-var randomGemValue = gemValue[Math.floor(Math.random() * 13) + 1];
+var randomRubyValue = [Math.floor(Math.random())];
+var randomSapphireValue = [Math.floor(Math.random())];
+var randomEmeraldValue = [Math.floor(Math.random())];
+var randomDiamondValue = [Math.floor(Math.random())];
 
-        
+
 var directionsText = document.getElementById("directions-text");
-var winsText = document.getElementById("wins-text"); 
+var winsText = document.getElementById("wins-text");
 var lossText = document.getElementById("losses-text");
 var numberText = document.getElementById("number-text");
 var scoreText = document.getElementById("score-text");
 
-function intializeGame(){
-    
-    wins = 0; 
+var ruby = document.getElementById("gem-1") || randomRubyValue;
+randomRubyValue = [Math.floor(Math.random())];
+
+
+var sapphire = document.getElementById("gem-2");
+var emerald = document.getElementById("gem-3");
+var diamond = document.getElementById("gem-4");
+
+
+
+function intializeGame() {
+
+    wins = 0;
     losses = 0;
     scoreTotal = 0;
-    
-    gemValue=[];
-    updateDom();
-    generateRandomTargetNum(19,120);
-
-    
+    generateRandomTargetNum(19, 120);
+    // generateRandomGemValue(1, 12);
+    console.log(randomTargetNumber);
+    // console.log(randomGemValue);
 };
 intializeGame();
 
-
-
-function resetGame(){
+function resetGame() {
     scoreTotal = 0;
-    gemValue=[];
+    // gemValue=[];
     updateDom();
     generateRandomTargetNum(19, 120);
+    console.log(randomTargetNumber);
+
+    // generateRandomGemValue(1, 12);
+    generateRandomRubyValue(1, 12);
+    generateRandomSapphireValue(1, 12);
+
+    // console.log(randomGemValue);
+    // console.log(generateRandomGemValue);
+    // console.log(randomRubyValue);
 
 };
 
 resetGame();
 
-function updateDom(){
-    
+function updateDom() {
+
     directionsText.textContent = ""
     winsText.textContent = "Wins: " + wins;
     lossText.textContent = "Losses: " + losses;
     numberText.textContent = "Target Number: " + randomTargetNumber;
     scoreText.textContent = "Your Score: " + scoreTotal;
-   
+
 }
 
 updateDom();
 
-function generateRandomTargetNum(min, max){
-    randomTargetNumber = Math.floor(Math.random() * (max-min)) + min;
-    
+function generateRandomTargetNum(min, max) {
+    randomTargetNumber = Math.floor(Math.random() * (max - min)) + min;
+
 };
 
 generateRandomTargetNum(19, 120);
 
+// function generateRandomGemValue(min, max) {
+//     randomRubyValue = Math.floor(Math.random() * (max - min)) + min;
+//     randomSapphireValue = Math.floor(Math.random() * (max - min)) + min;
+//     randomEmeraldValue = Math.floor(Math.random() * (max - min)) + min;
+//     randomDiamondValue = Math.floor(Math.random() * (max - min)) + min;
+//     console.log("ruby" + randomRubyValue);
+//     console.log("sapphire" + randomSapphireValue);
+// };
 
-function generateRandomGemValue() {
-    randomeGemValue = gemValue[Math.floor(Math.random() * 13) + 1];
+// generateRandomGemValue(1, 12);
+
+function generateRandomRubyValue(min, max) {
+    randomRubyValue = Math.floor(Math.random() * (max - min)) + min;
+
+    console.log("ruby" + randomRubyValue);
+
 };
 
-generateRandomGemValue();
+generateRandomRubyValue(1, 12);
 
-// click function below
+function generateRandomSapphireValue(min, max) {
 
-$(document).ready(function(){
-    $("img").click(function(){ //the "p" here is the 
-      alert("It worked!");
-      console.log("hi")
+    randomSapphireValue = Math.floor(Math.random() * (max - min)) + min;
+
+    console.log("sapphire" + randomSapphireValue);
+};
+
+generateRandomSapphireValue(1, 12);
+
+
+// click functions for each gem below.
+
+$(document).ready(function () {
+    $(ruby).click(function () {  
+
+    scoreTotal = scoreTotal + ruby //(the variable connected to the ruby)
+    $("#score-text, scoreText").html(scoreTotal); //(this connects the score to that html text)
+    // compareTargetNumberGemTotal ();  //(this calls out that if and else statement function into this gem function. Do that same for other gems)
+
+        alert("It worked!");
+        console.log("ruby")
+
     });
-  });
+
+    $(sapphire).click(function () { 
+
+    
+    scoreTotal = scoreTotal + sapphire 
+    $("#score-text").html(scoreTotal); 
+    // compareTargetNumberGemTotal ();  
+
+        alert("It worked!");
+        console.log("sapphire")
+
+    });
+
+    $(emerald).click(function () {  
+
+    scoreTotal = scoreTotal + emerald 
+    $("#score-text").html(scoreTotal); 
+    // compareTargetNumberGemTotal (); 
+
+        alert("It worked!");
+        console.log("emerald")
+
+    });
+
+    $(diamond).click(function () {  
+
+    scoreTotal = scoreTotal + diamond 
+    $("#score-text").html(scoreTotal); 
+    // compareTargetNumberGemTotal ();  
+
+        alert("It worked!");
+        console.log("diamond")
+
+    });
+});
+
+gemValues();
+
+function gemValues() {
+    if (randomRubyValue === randomSapphireValue) {
+        generateRandomSapphireValue(1, 12);
+    }
+}
 
 
 
+scoring();
 
-function compareTargetNumberGemTotal(){
+function scoring() {
+    if ($(ruby).click(function(){})) {
+        scoreTotal += randomRubyValue;
+};
+
+// Click/Scoring
+// 1: When you click on a gem, the score will increase.
+// 2: Each gem should have a different value and one gem must have a value of one.
+
+
+function compareTargetNumberGemTotal() {
 
     // if(compChoice.indexOf(userGuess) !== -1){
     //     console.log(userGuess);
-        if (scoreTotal === randomTargetNumber) {
-            wins++;
-            updateDom();
-            resetGame();
-            generateRandomTargetNum();
-            generateRandomGemValue();
-           
-        } else if (scoreTotal > randomTargetNumber) {
-            losses--;
-            updateDom();
-            resetGame();
-            generateRandomTargetNum();
-            generateRandomGemValue();
-            
-        }
+    if (scoreTotal === randomTargetNumber) {
+        wins++;
+        updateDom();
+        resetGame();
+        generateRandomTargetNum();
+        generateRandomGemValue();
+
+    } else if (scoreTotal > randomTargetNumber) {
+        losses--;
+        updateDom();
+        resetGame();
+        generateRandomTargetNum();
+        generateRandomGemValue();
+
+    }
 }
 
 compareTargetNumberGemTotal();
-
-
-
 
 // 2: During the game:
     // a: Once a gem is clicked, the value of that gem increases score. The value for each gem is different.
@@ -119,3 +210,4 @@ compareTargetNumberGemTotal();
         // -Resets the player score to 0.
         // -Randomly sets the target number value.
         // -Randomly sets the value of each gem. 
+}
